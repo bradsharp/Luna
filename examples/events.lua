ac9668909d82c9a5ef43082c6333025d35c9cdb3
Brad Sharp <BradSharp@users.noreply.github.com>
2016-11-27 00:53:30 +0000

    Create events.lua

---
+++ examples/events.lua
@@ -0,0 +1,23 @@
+local class = require'lib/class'
+
+local event do
+    event = class {
+        Connect = function (this, func)
+            this.signals[func] = coroutine.wrap(func)
+            return function ()
+                this.signals[func] = nil
+            end
+        end;
+        Disconnect = function (this, func)
+            this.signals[func] = nil
+        end;
+        Raise = function (this, ...)
+            for func, thread in ipairs(this.signals) do
+                thread(...)
+            end
+        end;
+        __construct = function (this)
+            this.signals = {}
+        end,
+    }
+end
