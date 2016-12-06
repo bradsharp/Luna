f0516ab50e9827aa3f677c72f30639ad6bb397ed
Brad Sharp <BradSharp@users.noreply.github.com>
2016-12-06 00:48:08 +0000

    Update events.lua

--- examples/events.lua
+++ examples/events.lua
@@ -1,19 +1,37 @@
 local class = require'lib/luna'
 
+local signal = class {
+	__construct = function (this, callback)
+		this.callback = callback
+	end;
+	Disconnect = function (this)
+		this.Connected = false
+		this.callback = nil
+	end;
+	Raise = function (this, ...)
+		if this.Connected and this.callback then
+			coroutine.wrap(this.callback)(...)
+		else
+			error("Signal has been disconnected")	
+		end
+	end;
+	Connected = true
+}
+
 local event do
     event = class {
         Connect = function (this, func)
-            this.signals[func] = coroutine.wrap(func)
-            return function () -- Return a disconnect function incase func was anonymous 
-                this.signals[func] = nil
-            end
-        end;
-        Disconnect = function (this, func)
-            this.signals[func] = nil
+			local newSignal = signal(func)
+			table.insert(this.signals, newSignal)
+            return newSignal
         end;
         Raise = function (this, ...)
-            for func, thread in pairs(this.signals) do
-                thread(...)
+            for i, signal in ipairs(this.signals) do
+                if signal.Connected then
+					signal:Raise(...)
+				else
+					this.signals[i] = nil
+				end
             end
         end;
         __construct = function (this)
