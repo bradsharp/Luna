1761d62e86d7baae93eae5c9c7ccdd53cdb63e02
Brad Sharp <brad.z.sharp@gmail.com>
2016-12-07 20:57:59 +0000

    Added stack example

---
+++ examples/stack.lua
@@ -0,0 +1,30 @@
+local class = require'lib/luna'
+
+local stack = class {
+
+    Size = {
+        get = function (this)
+            return #this.stack
+        end
+    };
+
+    __construct = function (this, ...)
+        this.stack = {...}
+    end;
+
+    Push = function (this, value)
+        table.insert(this.stack, value)
+    end;
+
+    Pop = function (this)
+        local value = this.stack[this.Size]
+        table.remove(this.stack, this.Size)
+        return value
+    end;
+
+    Peak = function (this)
+        local value = this.stack[this.Size]
+        return value
+    end;
+
+}
